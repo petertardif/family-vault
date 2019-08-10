bf029ab4f29a06a05ef421ddee5b3485c4693769
Peter Tardif <peter.tardif@gmail.com>
2019-08-10 00:01:21 -0600

    added reusable toggle component

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import MemoryList from '../MemoryList/MemoryList';
-import MemoryStore from '../fake-data-store'
+import MemoryStore from '../fake-data-store';
 
 function App() {
-  console.log(MemoryStore);
   return (
     <main className='App'>
-      {/* <Switch> */}
-        {/* <Route exact path='/' component={SampleComponent} /> */}
+      <Switch>
+        <Route 
+          exact path='/' 
+          render={() => 
+            <MemoryList 
+              memories={MemoryStore}
+            />} />
         {/* <Route path='/about' component={SampleComponent2} */}
         {/* <Route component={NotFoundPage} /> //THIS MUST BE AT THE BOTTOM OR IT WILL ALWAYS RENDER*/}
-      {/* </Switch> */}
-      <MemoryList memories={MemoryStore}/>
+      </Switch>
     </main>
   );
 }
--- src/Memory/Memory.js
+++ src/Memory/Memory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Memory.css';
+import Toggle from '../Toggle/Toggle';
 
 export default function Memory(props) {
   return (
@@ -13,30 +14,41 @@ export default function Memory(props) {
               Modified <span className='Date'>{props.memory_date}</span> 
           </div>
         </div>
-        <img src={props.media_url} alt={`supporting visual for ${props.title}`}/> 
-        <p className='Memory_description'>
-          {props.memory_desc}
-        </p>
-        <div className='Memory_buttons'>
-          <button
-            className='Btn_memory_edit'
-            // onClick={() => props.onClickEdit(props.id)}
-          >
-            Edit
-          </button>
-          <button
-            className='Btn_memory_delete'
-            // onClick={() => props.onClickDelete(props.id)}
-          >
-            Delete
-          </button>
-          <button
-            className='Btn_memory_back'
-            // onClick={() => props.onClickBack(props.id)}
-          >
-            Back
-          </button>
-        </div>
+        <Toggle>
+          {({ on, toggle }) => (
+            <>
+              {on && 
+                <>
+                  <img src={props.media_url} alt={`supporting visual for ${props.title}`}/>
+                  <p className='Memory_description'>
+                    {props.memory_desc}
+                  </p>
+                  <div className='Memory_buttons'>
+                    <button
+                      className='Btn_memory_edit'
+                      // onClick={() => props.onClickEdit(props.id)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className='Btn_memory_delete'
+                      // onClick={() => props.onClickDelete(props.id)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className='Btn_memory_back'
+                      // onClick={() => props.onClickBack(props.id)}
+                    >
+                      Back
+                    </button>
+                  </div>
+                </>
+              }
+              <button onClick={toggle}>Expand Memory</button>
+            </>
+          )}
+        </Toggle>
       </div>
     </li>
   )
