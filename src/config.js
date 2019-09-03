e9ab4b2b03c5bb332c29286d55de27004c73d634
Peter Tardif <peter.tardif@gmail.com>
2019-09-03 00:26:25 -0600

    fixing bugs

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://agile-fortress-94521.herokuapp.com/api`,
-  AWS_BASE_URL: process.env.REACT_APP_AWS_BASE_URL || `https://agile-fortress-94521.herokuapp.com/`,
+  AWS_BASE_URL: process.env.REACT_APP_AWS_BASE_URL || `https://agile-fortress-94521.herokuapp.com`,
 }
