c2bbc25f92e40675d2f64448d479740d8a5fcfe7
Peter Tardif <peter.tardif@gmail.com>
2019-09-20 15:19:07 -0600

    fixed the spinning bug on svgs

--- src/Spinner/Spinner.js
+++ src/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import './Spinner.css';
 
 export default () => 
   <>
-    <svg viewBox="25 25 50 50">
+    <svg className='spinner' viewBox="25 25 50 50">
       <circle cx="50" cy="50" r="20"></circle>
     </svg>
   </>
